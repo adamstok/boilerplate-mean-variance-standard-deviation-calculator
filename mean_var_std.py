3e340383185f6d02c0a5e670d3808799252d5e1c
adamstok <adamstokowski@outlook.com>
2021-01-05 21:20:50 +0100

    mean

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,5 +2,21 @@ import numpy as np
 
 
 def calculate(listed):
-    pass
+    if len(listed) != 9:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        arr = np.array(listed).reshape(3, 3)
+
+        outp = {
+            'mean': [],
+            'variance': [],
+            'standard deviation': [],
+            'max': [],
+            'min': [],
+            'sum': []
+        }
+
+        outp['mean'].append(list(arr.mean(axis=0)))
+        outp['mean'].append(list(arr.mean(axis=1)))
+        outp['mean'].append(arr.flatten().mean())
 
